dff27371a170865dfe02b175eedca0ee3492864e
Justin Sanorjo <justin.sanorjo@gmail.com>
2021-12-03 14:40:51 +0800

    end of section 12

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -55,7 +55,7 @@ export class PhotoEditorComponent implements OnInit {
       url: this.baseUrl + 'users/add-photo',
       authToken: 'Bearer ' + this.user?.token,
       isHTML5: true,
-      allowedFileType: ['img'],
+      allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024
@@ -67,8 +67,13 @@ export class PhotoEditorComponent implements OnInit {
 
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       if(response) {
-        const photo = JSON.parse(response);
+        const photo: Photo = JSON.parse(response);
         this.member.photos.push(photo);
+        if (photo.isMain) {
+          this.user.photoUrl =photo.url;
+          this.member.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+        }
       }
     }
   }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,21 +11,48 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  registerForm!: FormGroup;
+  maxDate?: Date;
+  validationErrors: string[] = [];
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18)
   }
 
+  initializeForm() {
+    this.registerForm =  this.fb.group({
+        gender: ['male'],
+        username: ['', Validators.required],
+        knownAs: ['', Validators.required],
+        dateOfBirth: ['', Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+        confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      if (control.parent && control.parent.controls) {
+        return control.value === (control.parent.controls as { [key: string]: AbstractControl })[matchTo].value
+          ? null : { isMatching: true };
+      }
+      return null;
+    }
+  } 
+
   register() {
-     this.accountService.register(this.model).subscribe({
-       next: response => {console.log(response); this.cancel();},
-       error: error => {console.log(error), this.toastr.error(error)}
+     this.accountService.register(this.registerForm.value).subscribe({
+       next: response => {this.router.navigateByUrl('/members');},
+       error: error => {this.validationErrors = error;}
       });
   }
   cancel() {
       this.cancelRegister.emit(false);
   }
-
 }
